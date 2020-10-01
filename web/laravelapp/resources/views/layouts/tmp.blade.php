9c5a9e6c3110a8a9d8845064bf5c14476b5e34e1
s18120233 <70698125+s18120233@users.noreply.github.com>
2020-10-01 09:39:37 +0900

    Add files via upload

--- web/laravelapp/resources/views/layouts/tmp.blade.php
+++ web/laravelapp/resources/views/layouts/tmp.blade.php
@@ -8,6 +8,9 @@
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
         
+        <!-- views CSS -->
+       	<link rel="stylesheet" type="text/css" href="css/club.blade.css">        
+
         <title>体育館管理</title>
     </head>
     <body>

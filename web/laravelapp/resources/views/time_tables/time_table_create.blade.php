17a7e4c54bbc55852e2009a21c5d0000107ae397
takepi5963 <tjichuan44@gmail.com>
2020-10-20 23:24:28 +0900

    master

--- web/laravelapp/resources/views/time_tables/time_table_create.blade.php
+++ web/laravelapp/resources/views/time_tables/time_table_create.blade.php
@@ -4,23 +4,31 @@
 <form action="/time_table/create" method="post">
 @csrf
 
-<div class="col-sm">
-<label for="time_num">一日当たりの枠数:</label>
-<input type="number" min="1" name="time_num" id="time_num" class="form-control">
+<div class="col-sm form-group row">
+    <div class="col-sm-3">
+    <label for="time_num" class="text-nowrap">一日当たりの時間数:</label>
+    <input type="number" min="1" name="time_num" id="time_num" value="4" class="form-control d-inline">
+    </div>
+    <div class="col-sm-3">
+    <label for="request_limit" class="text-nowrap">希望申し込みの上限:</label>
+    <input type="number" min="1" name="request_limit" id="request_limit" value="5" class="form-control d-inline">
+    </div>
 </div>
 
-<div class="col-sm">
-
-<label for="start_day">開始日付:</label>
-<input type="date" name="start_day" id="start_day" class="form-control">
-
-<label for="end_day">終了日付:</label>
-<input type="date" name="end_day" id="end_day" class="form-control">
+<div class="col-sm form-group row">
+        <div class="col-sm-5">
+            <label for="start_day">開始日付:</label>
+            <input type="date" name="start_day" id="start_day" class="form-control d-inline">
+        </div>
+        <div class="col-sm-5">
+            <label for="end_day">終了日付:</label>
+            <input type="date" name="end_day" id="end_day" class="form-control d-inline">
+        </div>
 </div>
 
 <div class="col-sm">
 <label for="exampleFormControlTextarea1">補足説明:</label>
-<textarea maxlength="100000" name="message" id="exampleFormControlTextarea1" class="form-control class="message"" rows="10"></textarea>
+<textarea maxlength="100000" name="message" id="exampleFormControlTextarea1" class="form-control message" rows="10"></textarea>
 </div>
 
 <input type="button" value="確定" class="btn btn-secondary" id="create_button">
